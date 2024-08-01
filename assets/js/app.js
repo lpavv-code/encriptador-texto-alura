4f7564e1e94400f1020daeaf130830c008e1aa59
Patrick Vigo <patrickvilchezv@gmail.com>
2024-08-01 02:00:19 -0500

    Corrigiendo todo el codigo para Movil First

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,71 +1,86 @@
-document.addEventListener("DOMContentLoaded", (event) => {
-  const textarea = document.querySelector(".main__textarea");
-  const encryptButton = document.querySelector(".main__button--encrypt");
-  const decryptButton = document.querySelector(".main__button--decrypt");
-  const resultTextarea = document.querySelector(".result__textarea");
-  const asideContainer = document.querySelector(".aside__container");
-  const asideResult = document.querySelector(".aside__result");
-  const copyButton = document.querySelector(".result__copy");
+const textoIngreso = document.querySelector(".text-ingreso");
+const textoResultado = document.querySelector(".texto-resultado");
+const seccionEspera = document.getElementById("espera");
+const seccionResultado = document.getElementById("resultado");
 
-  // Función para validar el texto en tiempo real
-  textarea.addEventListener("input", () => {
-    const value = textarea.value;
-    const invalidChars = /[^a-z\s]/g;
+document.querySelector(".boton-copiar").addEventListener("click", copiarTexto);
 
-    if (invalidChars.test(value)) {
-      alert("Solo se permiten letras minúsculas y sin acentos.");
-      textarea.value = value.replace(invalidChars, "");
+function copiarTexto() {
+  const texto = textoResultado.value;
+  navigator.clipboard
+    .writeText(texto)
+    .then(() => {
+      alert("Texto copiado al portapapeles");
+    })
+    .catch((err) => {
+      console.error("Error al copiar el texto: ", err);
+    });
+}
+
+function encriptar(parametroEncriptado) {
+  let codificacion = [
+    ["e", "enter"],
+    ["i", "imes"],
+    ["a", "ai"],
+    ["o", "ober"],
+    ["u", "ufat"],
+  ];
+  parametroEncriptado = parametroEncriptado.toLowerCase();
+
+  for (let i = 0; i < codificacion.length; i++) {
+    if (parametroEncriptado.includes(codificacion[i][0])) {
+      parametroEncriptado = parametroEncriptado.replaceAll(
+        codificacion[i][0],
+        codificacion[i][1]
+      );
     }
-  });
+  }
+  return parametroEncriptado;
+}
 
-  // Función para encriptar el texto
-  encryptButton.addEventListener("click", () => {
-    let text = textarea.value;
-    if (!text) return;
+function btnEncriptar() {
+  const texto = textoIngreso.value;
+  if (texto === "") {
+    alert("Ingresa un texto para poder encriptar");
+    return;
+  }
 
-    let encryptedText = text
-      .replace(/e/g, "enter")
-      .replace(/i/g, "imes")
-      .replace(/a/g, "ai")
-      .replace(/o/g, "ober")
-      .replace(/u/g, "ufat");
+  if (!/^[a-z\s]+$/.test(texto)) {
+    alert("Solo letras minúsculas y sin acentos");
+    return;
+  }
 
-    resultTextarea.value = encryptedText;
-    textarea.value = "";
+  const textoEncriptado = encriptar(texto);
+  textoResultado.value = textoEncriptado;
+  textoIngreso.value = "";
 
-    asideContainer.classList.add("hide");
-    asideResult.classList.remove("hide");
-  });
+  seccionEspera.classList.add("hide");
+  seccionResultado.classList.remove("hide");
+}
 
-  // Función para desencriptar el texto
-  decryptButton.addEventListener("click", () => {
-    let text = textarea.value;
-    if (!text) return;
+function desencriptar(parametroDesencriptado) {
+  let codificacion = [
+    ["e", "enter"],
+    ["i", "imes"],
+    ["a", "ai"],
+    ["o", "ober"],
+    ["u", "ufat"],
+  ];
+  parametroDesencriptado = parametroDesencriptado.toLowerCase();
 
-    let decryptedText = text
-      .replace(/enter/g, "e")
-      .replace(/imes/g, "i")
-      .replace(/ai/g, "a")
-      .replace(/ober/g, "o")
-      .replace(/ufat/g, "u");
+  for (let i = 0; i < codificacion.length; i++) {
+    if (parametroDesencriptado.includes(codificacion[i][1])) {
+      parametroDesencriptado = parametroDesencriptado.replaceAll(
+        codificacion[i][1],
+        codificacion[i][0]
+      );
+    }
+  }
+  return parametroDesencriptado;
+}
 
-    resultTextarea.value = decryptedText;
-    textarea.value = "";
-
-    asideContainer.classList.add("hide");
-    asideResult.classList.remove("hide");
-  });
-
-  // Función para copiar el texto
-  copyButton.addEventListener("click", () => {
-    const textToCopy = resultTextarea.value;
-    navigator.clipboard
-      .writeText(textToCopy)
-      .then(() => {
-        alert("Texto copiado al portapapeles");
-      })
-      .catch((err) => {
-        console.error("Error al copiar el texto: ", err);
-      });
-  });
-});
+function btnDesencriptar() {
+  const textoDesencriptado = desencriptar(textoIngreso.value);
+  textoResultado.value = textoDesencriptado;
+  textoIngreso.value = "";
+}
